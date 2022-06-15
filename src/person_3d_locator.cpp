47d81664bb8d50348ef2aa99951c3857389b1e3a
yakir <yakir@cogniteam.com>
2022-06-15 16:24:13 +0300

    wip

--- src/person_3d_locator.cpp
+++ src/person_3d_locator.cpp
@@ -654,8 +654,8 @@ void Person3DLocator::PublishRotationGoal()
     float fTargetDepthRayDistance{targetedPersonYakir_.distnace_ - fRecoveryTargetOffset};
     geometry_msgs::PoseStamped poseMsg;
 
-    float angleFromTarget =  atan2(targetedPersonYakir_.location_.y,
-        targetedPersonYakir_.location_.x);
+    float angleFromTarget =  atan2(targetedPersonYakir_.location_.pose.position.y,
+        targetedPersonYakir_.location_.pose.position.x);
 
     // PrepareTargetPoseFromDepthRay(poseMsg, fTargetDepthRayDistance, personCameraTarget.angleX, personCameraTarget.x);
     PrepareTargetPoseFromDepthRay(poseMsg, fTargetDepthRayDistance, angleFromTarget,targetedPersonYakir_.box_.bbox.center.x);
